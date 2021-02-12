342c92d31c66bc09052a49f5641174398ab3025b
GAHYUN LEE <gahyunlee26@gmail.com>
2021-02-11 16:34:10 -0800

    single line avatar selection list created need unselect color fix

--- src/app/components/lists/single-line-avatar-selection-list/single-line-avatar-selection-list.component.ts
+++ src/app/components/lists/single-line-avatar-selection-list/single-line-avatar-selection-list.component.ts
@@ -9,18 +9,30 @@ export class SingleLineAvatarSelectionListComponent implements OnInit {
   listItems = [
     {
       avatar: '../../../../assets/images/avatar.jpeg',
-      itemText: 'Single-line item',
+      itemText: 'Single-line item1',
     },
     {
       avatar: '../../../../assets/images/avatar.jpeg',
-      itemText: 'Single-line item',
+      itemText: 'Single-line item2',
     },
     {
       avatar: '../../../../assets/images/avatar.jpeg',
-      itemText: 'Single-line item',
+      itemText: 'Single-line item3',
     },
   ];
+
+  selectedItems = [];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelectionChange(event, selected) {
+    this.selectedItems = []; // clear
+    selected.forEach((listItem) => {
+      console.log(listItem.value);
+      this.selectedItems.push(listItem.value);
+    });
+    console.log(this.selectedItems);
+  }
 }
