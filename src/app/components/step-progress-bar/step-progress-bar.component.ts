17f7617bfba578e63acb11f6711c2e4d35ac83b5
GAHYUN LEE <gahyunlee26@gmail.com>
2021-03-02 15:25:03 -0800

    step-progress-bar component seperated

--- src/app/components/step-progress-bar/step-progress-bar.component.ts
+++ src/app/components/step-progress-bar/step-progress-bar.component.ts
@@ -1,28 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkStep, CdkStepper } from '@angular/cdk/stepper';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // for step progress bar
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+
 @Component({
   selector: 'app-step-progress-bar',
   templateUrl: './step-progress-bar.component.html',
   styleUrls: ['./step-progress-bar.component.scss'],
-  providers: [{ provide: CdkStepper, useExisting: StepProgressBarComponent }],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { displayDefaultIndicatorType: false },
+    },
+  ],
 })
-export class StepProgressBarComponent extends CdkStepper implements OnInit {
-  // Approach1)
-  // step1Label: string = 'Label1 here';
-  // step2Label: string = 'Label2 here';
-  // step3Label: string = 'Label3 here';
+export class StepProgressBarComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  isEditable = true;
 
-  // Approach2) https://indepth.dev/posts/1284/building-a-custom-stepper-using-angular-cdk
-  // how to build a custom stepper wizard using angular material-cdk: https://www.mokkapps.de/blog/how-i-built-a-custom-stepper-wizard-using-angular-material-cdk/
-  // example with angular material cdk stepper: https://stackblitz.com/edit/angular-basic-cdk-stepper?embed=1&file=src/app/custom-stepper/custom-stepper.component.ts
-  // Angular Material - Creating a custom stepper using the CDK stepper: https://material.angular.io/guide/creating-a-custom-stepper-using-the-cdk-stepper
-  // custom wizard progress stepper component in angular: https://youtu.be/BVlpD1PaF58
-
-  onClick(index: number): void {
-    this.selectedIndex = index;
-  }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    console.log(this._steps);
+    this.firstFormGroup = this.formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
   }
 }
---
+++ src/app/components/temp/step-progress-bar/step-progress-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CdkStep, CdkStepper } from '@angular/cdk/stepper';
+@Component({
+  selector: 'app-step-progress-bar',
+  templateUrl: './step-progress-bar.component.html',
+  styleUrls: ['./step-progress-bar.component.scss'],
+  providers: [{ provide: CdkStepper, useExisting: StepProgressBarComponent }],
+})
+export class StepProgressBarComponent extends CdkStepper implements OnInit {
+  // Approach1)
+  // step1Label: string = 'Label1 here';
+  // step2Label: string = 'Label2 here';
+  // step3Label: string = 'Label3 here';
+
+  // Approach2) https://indepth.dev/posts/1284/building-a-custom-stepper-using-angular-cdk
+  // how to build a custom stepper wizard using angular material-cdk: https://www.mokkapps.de/blog/how-i-built-a-custom-stepper-wizard-using-angular-material-cdk/
+  // example with angular material cdk stepper: https://stackblitz.com/edit/angular-basic-cdk-stepper?embed=1&file=src/app/custom-stepper/custom-stepper.component.ts
+  // Angular Material - Creating a custom stepper using the CDK stepper: https://material.angular.io/guide/creating-a-custom-stepper-using-the-cdk-stepper
+  // custom wizard progress stepper component in angular: https://youtu.be/BVlpD1PaF58
+
+  onClick(index: number): void {
+    this.selectedIndex = index;
+  }
+
+  ngOnInit(): void {
+    console.log(this._steps);
+  }
+}
