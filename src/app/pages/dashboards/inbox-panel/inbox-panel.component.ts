e9bb3fcdb2976957d8534f74fc8a73bfd9c48be9
GAHYUN LEE <gahyunlee26@gmail.com>
2021-04-26 22:25:04 -0700

    inbox-panel created

--- src/app/pages/dashboards/inbox-panel/inbox-panel.component.ts
+++ src/app/pages/dashboards/inbox-panel/inbox-panel.component.ts
@@ -9,18 +9,33 @@ export class InboxPanelComponent implements OnInit {
   listItems = [
     {
       avatar: '../../../../assets/images/avatar.jpeg',
-      itemText: 'Single-line item1',
-      secondaryText: 'secondary item1',
+      name: 'Nilesh Dubey',
+      message: 'Hello, are you available now?',
+      time: '12:00 PM',
     },
     {
       avatar: '../../../../assets/images/avatar.jpeg',
-      itemText: 'Single-line item2',
-      secondaryText: 'secondary item1',
+      name: 'Nancee Dalton',
+      message: 'Have you checked my email?',
+      time: '12:00 PM',
     },
     {
       avatar: '../../../../assets/images/avatar.jpeg',
-      itemText: 'Single-line item3',
-      secondaryText: 'secondary item1',
+      name: 'Mabelle Doyland',
+      message: 'Please wait!!',
+      time: '12:00 PM',
+    },
+    {
+      avatar: '../../../../assets/images/avatar.jpeg',
+      name: 'Janeen Carnow',
+      message: 'Can I ask you something?',
+      time: '12:00 PM',
+    },
+    {
+      avatar: '../../../../assets/images/avatar.jpeg',
+      name: 'Pancho Mazillius',
+      message: 'Hey! How are you?',
+      time: '12:00 PM',
     },
   ];
   constructor() {}
