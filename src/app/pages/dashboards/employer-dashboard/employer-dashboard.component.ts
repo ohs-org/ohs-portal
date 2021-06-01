3afb86b51b512310436de00ce8e51289864bd41f
GAHYUN LEE <gahyunlee26@gmail.com>
2021-06-01 11:45:41 -0700

    employer dashboard tables fetching and filtering correct data

--- src/app/pages/dashboards/employer-dashboard/employer-dashboard.component.ts
+++ src/app/pages/dashboards/employer-dashboard/employer-dashboard.component.ts
@@ -1,5 +1,13 @@
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 import { DropDownOption } from './../../../models/dropdown-option.model';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  QueryList,
+  ViewChildren,
+  AfterViewInit,
+} from '@angular/core';
 import { ActionItemService } from './../../../services/action-item.service';
 import { ActionItem } from './../../../models/action-item.model';
 import { ChartType } from 'chart.js';
@@ -11,7 +19,7 @@ import * as moment from 'moment';
   templateUrl: './employer-dashboard.component.html',
   styleUrls: ['./employer-dashboard.component.scss'],
 })
-export class EmployerDashboardComponent implements OnInit {
+export class EmployerDashboardComponent implements OnInit, AfterViewInit {
   // User Info
   user: any = {
     first_name: 'Craig',
@@ -64,6 +72,37 @@ export class EmployerDashboardComponent implements OnInit {
     { value: '1', viewValue: 'Work BC Claim 4' },
   ];
 
+  // TABLE DATA
+  myDisplayedColumns: string[] = ['action-title', 'due', 'priority', 'status'];
+  displayedColumns: string[] = [
+    'action-title',
+    'due',
+    'priority',
+    'assigned-to',
+    'status',
+  ];
+
+  // overdue table
+  // ** paginator was undefined when try to get with ViewChild because ngIf mess things up
+  // ** in this case, use ViewChildren
+  @ViewChildren('myOverduePaginator')
+  myOverduePaginator: QueryList<MatPaginator>;
+  myActionItemsOverdue: ActionItem[];
+  myOverdueDS = new MatTableDataSource<ActionItem>();
+  // upcoming deadline table
+  @ViewChildren('myUpcomingPaginator')
+  myUpcomingPaginator: QueryList<MatPaginator>;
+  myActionItemsUpcomingDeadline: ActionItem[];
+  myUpcomingDeadlineDS = new MatTableDataSource<ActionItem>();
+
+  @ViewChildren('overduePaginator') overduePaginator: QueryList<MatPaginator>;
+  actionItemsOverdue: ActionItem[];
+  overdueDS = new MatTableDataSource<ActionItem>();
+  // upcoming deadline table
+  @ViewChildren('upcomingPaginator') upcomingPaginator: QueryList<MatPaginator>;
+  actionItemsUpcomingDeadline: ActionItem[];
+  upcomingDeadlineDS = new MatTableDataSource<ActionItem>();
+
   constructor(private actionItemService: ActionItemService) {}
 
   ngOnInit(): void {
@@ -72,8 +111,10 @@ export class EmployerDashboardComponent implements OnInit {
       this.filterActionItemWithinWeek(data);
       this.filterActionItemDueToday(data);
       // table items
-      // this.filterActionItemOverdue(data);
-      // this.filterActionItemUpcomingDeadline(data);
+      this.filterMyActionItemOverdue(data);
+      this.filterMyActionItemUpcomingDeadline(data);
+      this.filterActionItemOverdue(data);
+      this.filterActionItemUpcomingDeadline(data);
 
       this.weekChartData = this.calculateStatusChartData(
         this.actionItemsDueWeek
@@ -82,6 +123,30 @@ export class EmployerDashboardComponent implements OnInit {
         this.actionItemsDueToday
       );
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.myOverduePaginator.changes.subscribe(
+      (paginator: QueryList<MatPaginator>) => {
+        this.setPaginator(this.myOverdueDS, paginator.first);
+      }
+    );
+    this.myUpcomingPaginator.changes.subscribe(
+      (paginator: QueryList<MatPaginator>) => {
+        this.setPaginator(this.myUpcomingDeadlineDS, paginator.first);
+      }
+    );
+
+    this.overduePaginator.changes.subscribe(
+      (paginator: QueryList<MatPaginator>) => {
+        this.setPaginator(this.overdueDS, paginator.first);
+      }
+    );
+    this.upcomingPaginator.changes.subscribe(
+      (paginator: QueryList<MatPaginator>) => {
+        this.setPaginator(this.upcomingDeadlineDS, paginator.first);
+      }
+    );
   }
 
   // FUNCTIONS FOR CHARTS
@@ -129,4 +194,85 @@ export class EmployerDashboardComponent implements OnInit {
       (completed / itemCount) * 100,
     ];
   };
+
+  // FUNCTIONS FOR TABLES
+  // filter overdue items
+  filterMyActionItemOverdue = (data: ActionItem[]) => {
+    this.myActionItemsOverdue = data.filter((actionItem) => {
+      return (
+        actionItem.assignedTo ===
+          `${this.user.first_name} ${this.user.last_name}` &&
+        moment(actionItem.due).isBefore(this.today, 'day') &&
+        actionItem.status != 'completed'
+      );
+    });
+    this.setDataSource(this.myActionItemsOverdue, this.myOverdueDS);
+  };
+
+  filterActionItemOverdue = (data: ActionItem[]) => {
+    this.actionItemsOverdue = data.filter((actionItem) => {
+      return (
+        moment(actionItem.due).isBefore(this.today, 'day') &&
+        actionItem.status != 'completed'
+      );
+    });
+    this.setDataSource(this.actionItemsOverdue, this.overdueDS);
+  };
+
+  // filter approaching deadline items
+  filterMyActionItemUpcomingDeadline = (data: ActionItem[]) => {
+    this.myActionItemsUpcomingDeadline = data.filter((actionItem) => {
+      return (
+        actionItem.assignedTo ===
+          `${this.user.first_name} ${this.user.last_name}` &&
+        moment(actionItem.due).isBetween(
+          this.yesterday,
+          this.inFourDays,
+          'day'
+        ) &&
+        actionItem.status != 'completed'
+      );
+    });
+    this.setDataSource(
+      this.myActionItemsUpcomingDeadline,
+      this.myUpcomingDeadlineDS
+    );
+  };
+
+  filterActionItemUpcomingDeadline = (data: ActionItem[]) => {
+    this.actionItemsUpcomingDeadline = data.filter((actionItem) => {
+      return (
+        moment(actionItem.due).isBetween(
+          this.yesterday,
+          this.inFourDays,
+          'day'
+        ) && actionItem.status != 'completed'
+      );
+    });
+    this.setDataSource(
+      this.actionItemsUpcomingDeadline,
+      this.upcomingDeadlineDS
+    );
+  };
+
+  // set array to data source
+  setDataSource = (
+    actionItems: ActionItem[],
+    dataSource: MatTableDataSource<ActionItem>
+  ) => {
+    dataSource.data = actionItems;
+  };
+
+  //set paginator
+  setPaginator = (
+    dataSource: MatTableDataSource<ActionItem>,
+    paginator: MatPaginator
+  ) => {
+    dataSource.paginator = paginator;
+  };
+
+  // date format
+  getDateFormat(date): string {
+    return moment(date).format('MMM DD, YYYY');
+  }
 }
