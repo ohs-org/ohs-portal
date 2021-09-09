43f5113b1277065f5fb5e1415027c80d2b4d723f
GAHYUN LEE <gahyunlee26@gmail.com>
2021-09-09 15:10:59 -0700

    action first letter capitalized and added duration

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -10,12 +10,21 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   encapsulation: ViewEncapsulation.None,
 })
 export class NotificationsComponent implements OnInit {
+  durationInSeconds = 4;
+
   constructor(private _snackBar: MatSnackBar) {}
 
   ngOnInit(): void {}
 
   openSnackBar = (message: string, action: string) => {
     // action first letter capitalize
-    this._snackBar.open(message, action);
+    const capitalizedAction = this.capitalizeFirstChar(action);
+    this._snackBar.open(message, capitalizedAction, {
+      duration: this.durationInSeconds * 1000,
+    });
   };
+
+  capitalizeFirstChar(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 }
