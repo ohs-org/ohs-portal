2200277046dd859d775965f2e9fde221db3785b9
GAHYUN LEE <gahyunlee26@gmail.com>
2021-09-16 12:04:57 -0700

    uploader component and page created and router set up

---
+++ src/app/pages/uploaders/uploaders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uploaders',
+  templateUrl: './uploaders.component.html',
+  styleUrls: ['./uploaders.component.scss']
+})
+export class UploadersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
