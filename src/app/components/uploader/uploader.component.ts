9cbd1c6466d870f4005e74e34247b6c40f76bbec
GAHYUN LEE <gahyunlee26@gmail.com>
2021-09-16 12:08:17 -0700

    uploader reference link added

--- src/app/components/uploader/uploader.component.ts
+++ src/app/components/uploader/uploader.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-uploader',
   templateUrl: './uploader.component.html',
-  styleUrls: ['./uploader.component.scss']
+  styleUrls: ['./uploader.component.scss'],
 })
 export class UploaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
+
+// Drag and drop file uploader ref
+// Article: https://medium.com/@tarekabdelkhalek/how-to-create-a-drag-and-drop-file-uploading-in-angular-78d9eba0b854
+// Stackblitz: https://stackblitz.com/edit/angular-drag-n-drop-directive?file=angular.json
