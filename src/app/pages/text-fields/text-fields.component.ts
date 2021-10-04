63585c2bf2188db22058c956d16c5d024297ae9a
GAHYUN LEE <gahyunlee26@gmail.com>
2021-10-04 16:59:17 -0700

    different types of text boxes and error message showing with warning icon

--- src/app/pages/text-fields/text-fields.component.ts
+++ src/app/pages/text-fields/text-fields.component.ts
@@ -1,15 +1,26 @@
+import { FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text-fields',
   templateUrl: './text-fields.component.html',
-  styleUrls: ['./text-fields.component.scss']
+  styleUrls: ['./text-fields.component.scss'],
 })
 export class TextFieldsComponent implements OnInit {
+  hide = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  errorTxtInput = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+  ]);
+
+  getErrorMessage = () => {
+    return this.errorTxtInput.hasError
+      ? 'Error: <insert how to recover from error>'
+      : '';
+  };
 }
